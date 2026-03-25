4a9cc9ff44e0ca3b7086a6ad8cf462c484a44299
Kinjal Chandel <kinjalchandel2004@gmail.com>
2026-03-25 15:30:18 -0700

    tried fixing conflicts

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
 from app.api.routes.restaurant_route import router as restaurant_router
 from app.api.routes.notification_route import router as notification_router
 from app.api.routes.delivery_route import router as delivery_router
@@ -7,12 +7,10 @@ from app.api.routes.driver_route import router as driver_router
 
 app = FastAPI()
 
-router = APIRouter() # this is the main router for the application, it will be used to include all the other routers in the application
-router.include_router(restaurant_router) # this will include the restaurant router in the main router, so that all the routes defined in the restaurant router will be available in the main router
 app.include_router(restaurant_router)
 app.include_router(notification_router)
 app.include_router(delivery_router)
-app.include_router(driver_router) # this will include the driver router in the main router, so that all the routes defined in the driver router will be available in the main router
+app.include_router(driver_router)
 
 
 
