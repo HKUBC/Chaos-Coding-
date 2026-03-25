2d4167968f1fff9d14241451feba4681e8815953
Kobe <kobe.ch2068@gmail.com>
2026-03-24 20:35:34 -0700

    Refactoring order

--- app/model/order.py
+++ app/model/order.py
@@ -1,22 +1,18 @@
 from app.model.item import Item
 from app.model.order_status import OrderStatus
-from app.services.data_service import DataService
 
 class Order:
     """
     Manages order details, including items, and status.
-    Dependency on DataService to load delivery data from a CSV file.
     """
 
-    def __init__(self, order_id : str, customer_id : str, restaurant_id : str, data_service: DataService):
+    def __init__(self, order_id : str, customer_id : str, restaurant_id : str):
         self.order_id          = order_id
         self.customer_id       = customer_id
         self.restaurant_id     = restaurant_id
 
-        self.items: list[Item] = []                     # List of items
-
+        self.items: list[Item] = []                      # List of items
         self.status            = OrderStatus.CREATING    # Order status: creating, pending, preparing, delivered, cancelled
-        self._data_service     = data_service
 
     def add_item(self, item: Item):
         if self.status != OrderStatus.CREATING:
@@ -47,11 +43,6 @@ class Order:
 
         if not self.status.can_start():
             raise ValueError(f"Cannot start your order. Your order is currently {self.status}.")
-        
-        delivery_data = self._data_service.load_data()
-        if delivery_data:
-            row = delivery_data[1]
-            print(f"Starting your order {self.order_id} with delivery time: ", row.get('delivery_time', 'N/A'))
 
         self.update_status(OrderStatus.PENDING)
 
