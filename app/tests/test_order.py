077b5a926d757825a276a361076b1b17a15c640c
Kinjal Chandel <kinjalchandel2004@gmail.com>
2026-03-24 20:13:17 -0700

    Fixed the conflicts

--- app/tests/test_order.py
+++ app/tests/test_order.py
@@ -79,7 +79,7 @@ def test_order_total(order, item_example):
 def test_order_total_empty(order):
     assert order.order_total() == 0.0
 
-def teset_order_total_multiple_items(order, item_example):
+def test_order_total_multiple_items(order, item_example):
     item_example2 = MagicMock(spec=Item)
     item_example2.item_id = "def"
     item_example2.total_price.return_value = 4.99
